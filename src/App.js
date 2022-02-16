746508f712fb3a81c24251801d86184f68a1a7df
sudhir561 <sudhirme101@gmail.com>
2022-02-16 19:08:06 +0530

    form validation using custom hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import useForm from './Hooks/useForm';
 
 function App() {
+
+  //Final submit function
+  const formLogin = () => {
+
+    console.log("Callback function when form is submitted!");
+    console.log("Form Values ", values);
+  }
+
+  //Custom hook call
+  const {handleChange, values,errors,handleSubmit} = useForm(formLogin);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+      <input type="email" name="email" placeholder="E-mail"  onChange={handleChange}   />
+      {
+        errors.email && <h3>{errors.email}</h3>
+      }
+      <input minLength='8' type="password" name="password" placeholder="password"  onChange={handleChange}   />
+      {
+        errors.password && <h3>{errors.password}</h3>
+
+      }
+      <input type="text" minLength='5' required name="username" placeholder="username"  onChange={handleChange}   />
+      {
+        errors.username && <h3>{errors.username}</h3>
+
+      }
+      <input type="submit" value="Submit" className="submit"  />
+      </form>
+
     </div>
   );
 }
---
+++ src/Hooks/useForm.js
@@ -0,0 +1,127 @@
+import React,{useState} from "react";
+import {omit} from 'lodash';
+
+
+
+
+
+const useForm=(callback)=> {
+     //Form values
+const[values,setValues]= useState({})
+//Errors
+const[errors,setErrors]= useState({})
+
+ //A function to validate each input values
+
+ const validate=(event,name,value)=>{
+     switch(name){
+         case "username":{
+             if(value.length<=4){
+                   // we will set the error state
+
+                   setErrors({
+                       ...errors,
+                       username: "username should contain atleast 5 Letters"
+                   })
+             }
+             else{
+
+                // set the error state empty or remove the error for username input
+                //omit function removes/omits the value from given object and returns a new object
+ 
+                let newObj= omit(errors,"username")
+                setErrors(newObj);
+              
+ 
+              }
+            
+             
+         }
+         break;
+
+         case 'email':
+            if(
+                !new RegExp( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(value)
+            ){
+                setErrors({
+                    ...errors,
+                    email:'Enter a valid email address'
+                })
+            }else{
+
+                let newObj = omit(errors, "email");
+                setErrors(newObj);
+                
+            }
+        break;
+        
+        case 'password':
+            if(
+                !new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/).test(value)
+            ){
+                setErrors({
+                    ...errors,
+                    password:'Password should contains atleast 8 charaters and containing uppercase,lowercase and numbers'
+                })
+            }else{
+
+                let newObj = omit(errors, "password");
+                setErrors(newObj);
+                
+            }
+        break;
+
+
+         default:
+             break;
+     }
+ }
+
+
+
+
+
+
+//method to handle form input
+const handleChange=(event)=>{
+  //To stop default events    
+  event.persist();
+  
+  let name=event.target.name;
+  let val=event.target.value;
+
+  
+ //Let's set these values in state
+  setValues({
+      ...values,  // spread operator is use to store old values
+      [name]:val,
+      
+  })
+  validate(event,name,val);
+  
+}
+
+const handleSubmit = (event) => {
+    if(event) event.preventDefault();
+
+    if(Object.keys(errors).length === 0 && Object.keys(values).length !==0 ){
+        callback();
+
+    }else{
+        alert("There is an Error!");
+    }
+}
+
+    return ({
+        values,
+        errors,
+        handleChange,
+        handleSubmit
+
+    })
+
+    
+
+}
+
+export default useForm
